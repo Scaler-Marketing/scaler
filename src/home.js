9d6b447bab838846a9c77199d8db419b76b1ee0c
diegoliv-scaler <diego@scalermarketing.com>
2024-07-15 19:12:56 -0300

    added logic for services section

--- src/home.js
+++ src/home.js
@@ -38,3 +38,59 @@ document.querySelectorAll(".case-study-item").forEach((trigger) => {
     },
   });
 });
+
+// Services ================================== //
+const servicesItems = document.querySelectorAll('.services.list-item');
+
+servicesItems.forEach((item) => {
+  const tl = gsap.timeline();
+  const line = item.querySelector('.services-list-item-line'),
+    heading = item.querySelector('h3'),
+    description = item.querySelector('.services-list-item-description > p'),
+    button = item.querySelector('.button');
+  
+    const headingLines = new SplitType(heading, {
+      types: "lines, words",
+      tagName: "span",
+    });
+
+    const descriptionLines = new SplitType(description, {
+      types: "lines",
+      tagName: "span",
+    });
+  
+  let headingEls, descriptionEls;
+  
+  setLinesWrapper(headingLines.lines, () => {
+    headingEls = heading.querySelectorAll('.word');
+    gsap.set(headingEls, {yPercent: 100});
+  });
+  setLinesWrapper(descriptionLines.lines, () => {
+    descriptionEls = description.querySelectorAll(".line");
+    gsap.set(descriptionEls, { yPercent: 100 });
+  });
+
+  gsap.set(line, { xScale: 0 });
+  gsap.set(button, { yPercent: 100 });
+
+  tl.to(line, {
+    xPercent: 100,
+    duration: 0.5,
+    ease: "power4.Out",
+  }).to(headingEls, {
+    yPercent: 100,
+    stagger: 0.1,
+    duration: 1,
+    ease: "power4.Out",
+  }).to(descriptionEls, {
+    yPercent: 100,
+    stagger: 0.1,
+    duration: 1,
+    ease: "power4.Out",
+  }).to(button, {
+    yPercent: 100,
+    duration: 0.5,
+    ease: "power4.Out",
+  });
+  
+});
