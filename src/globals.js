724f79d49851dd76e235b0e01c294b2f01765579
diegoliv-scaler <diego@scalermarketing.com>
2024-12-18 17:29:10 -0300

    added contact form logic

--- src/globals.js
+++ src/globals.js
@@ -4,6 +4,7 @@ import { setLoadingStates } from "./modules/setLoadingStates";
 import { playVideoOnScroll }  from "./modules/playVideoOnScroll";
 import { initMenu } from "./modules/menu";
 import { setImageMasks } from "./modules/setImageMasks";
+import { initFormSubmit, initContactFormUi } from "./modules/formSubmit";
 
 initMenu();
 playVideoOnScroll();
@@ -13,4 +14,6 @@ setImageMasks();
 
 document.fonts.ready.then(() => {
   setStaggerText();
+  initFormSubmit();
+  initContactFormUi();
 });
---
+++ src/modules/formSubmit.js
@@ -0,0 +1,81 @@
+export function initFormSubmit() {
+  const forms = document.querySelectorAll("form");
+
+  if (!forms) {
+    return;
+  }
+
+  forms.forEach((form) => {
+    const formSubmitButton = form.querySelector(".button.is-submit");
+    const submitButton = form.querySelector(".form_submit");
+    const label = form.querySelector(".button-label");
+
+    if (!formSubmitButton || !submitButton) {
+      return;
+    }
+  
+    // add event listener to the form submit button
+    formSubmitButton.addEventListener("click", function (e) {
+      e.preventDefault(); // prevent the default action
+      // check if the form is valid
+      if (form.checkValidity()) {
+        // if valid, submit the form and change the button text
+        submitButton.click();
+        
+        label.textContent = submitButton.getAttribute("data-wait");
+      } else {
+        // if not valid, report validity (this will show the HTML5 validation messages)
+        form.reportValidity();
+      }
+    });
+  });
+}
+
+export function initContactFormUi() {
+  const formWrapper = document.querySelector(".contact-component");
+  const formTriggers = document.querySelectorAll("[data-contact-trigger]");
+  console.log("initContactFormUi", formTriggers, formWrapper);
+
+  if (!formWrapper || !formTriggers) {
+    return;
+  }
+
+
+  const formEl = formWrapper.querySelector(".contact-form"),
+    imgEl = formWrapper.querySelector(".contact-img_wrapper"),
+    closeBtn = formWrapper.querySelector('.button.is-modal-close');
+
+  gsap.set(formWrapper, { display: "none" });
+  gsap.set(formEl, { clipPath: "inset(0% 0% 100% 0%)" });
+  gsap.set(imgEl, { clipPath: "inset(100% 0% 0% 0%)" });
+
+  const tl = gsap.timeline({ paused: true });
+
+  tl.to(formWrapper, { display: "flex", duration: 0 })
+    .to(formEl, {
+      clipPath: "inset(0% 0% 0% 0%)",
+      ease: "power4.out",
+      duration: 0.75,
+    })
+    .to(
+      imgEl,
+      {
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "power4.out",
+        duration: 0.75,
+      },
+      "-=.75"
+    )
+
+  closeBtn.addEventListener("click", () => {
+    tl.reverse();
+  });
+
+  formTriggers.forEach((trigger) => {
+    console.log(trigger);
+    trigger.addEventListener("click", () => {
+      console.log('trigger click');
+      tl.play();
+    });
+  });
+}
