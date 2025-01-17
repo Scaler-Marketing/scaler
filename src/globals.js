b310d3852ac4f4fab21ebc15aff550a038ed29a1
diegoliv-scaler <diego@scalermarketing.com>
2025-01-17 13:10:24 -0300

    testing back button loading strategies

--- src/globals.js
+++ src/globals.js
@@ -6,6 +6,16 @@ import { initMenu } from "./modules/menu";
 import { setImageMasks } from "./modules/setImageMasks";
 import { initFormSubmit, initContactFormUi } from "./modules/formSubmit";
 
+window.addEventListener("beforeunload", () => {
+  console.log('beforeunload');
+  setLoadingStates();
+});
+
+window.addEventListener("popstate", () => {
+  console.log('popstate');
+  setLoadingStates();
+});
+
 document.addEventListener("pageshow", (event) => {
   console.log('pageshow', event);
   // Check if the page was restored from the bfcache
@@ -16,6 +26,7 @@ document.addEventListener("pageshow", (event) => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+  console.log('DOMContentLoaded');
   playVideoOnScroll();
   setStaggerElements();
   setImageMasks();
