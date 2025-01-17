c4cb14ea46871e01079d235ed78e5d5d7718a076
diegoliv-scaler <diego@scalermarketing.com>
2025-01-17 12:54:11 -0300

    updates to loading logic for back browser button

--- src/globals.js
+++ src/globals.js
@@ -1,11 +1,19 @@
 import { setStaggerText } from "./modules/staggerText";
 import { setStaggerElements } from "./modules/staggerElements";
 import { setLoadingStates } from "./modules/setLoadingStates";
-import { playVideoOnScroll }  from "./modules/playVideoOnScroll";
+import { playVideoOnScroll } from "./modules/playVideoOnScroll";
 import { initMenu } from "./modules/menu";
 import { setImageMasks } from "./modules/setImageMasks";
 import { initFormSubmit, initContactFormUi } from "./modules/formSubmit";
 
+document.addEventListener("pageshow", (event) => {
+  console.log('pageshow', event);
+  // Check if the page was restored from the bfcache
+  if (event.persisted) {
+    // Re-run your loading animation
+    setLoadingStates();
+  }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   playVideoOnScroll();
